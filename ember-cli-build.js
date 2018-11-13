817f16cb7f0438d2acbdce85b587e91a52116d12
Jacob Quant <jacobq@gmail.com>
2018-11-13 07:06:00 -0600

    Downgrade to ember(-cli,-data) 2.x

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,10 +5,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
-    // ember-auto-import
-    //autoImport: {
-    //  exclude: ['pouchdb'],
-    //}
   });
 
   // Use `app.import` to add additional libraries to the generated
